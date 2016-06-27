82ebd2d56d0632efeaffdab6bc1897a35c6d4ecd
Jon Petto <jonpetto@gmail.com>
2016-06-27 12:09:49 -0500

    WIP - Adding native ES6

---
+++ media/js/firefox/firstrun/firstrun-es6.js
@@ -0,0 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import FxaIframe from '../../base/mozilla-fxa-iframe-es6.js';
+
+FxaIframe.init({
+    gaEventName: 'firstrun-fxa'
+});
