8906e29921a5d1567071cd58faa0dc3ee68bafac
Jon Petto <jonpetto@gmail.com>
2017-08-07 15:39:46 -0500

    WIP [fix bug 1361092] Add referrer logic to Optimizely.

--- media/js/base/core-datalayer-page-id.js
+++ media/js/base/core-datalayer-page-id.js
@@ -26,11 +26,54 @@ if (typeof Mozilla == 'undefined') {
         var referrer;
 
         // if referrer cookie exists, store the value and remove the cookie
-        if (Mozilla.Cookies && Mozilla.Cookies.hasItem('mozilla-traffic-cop-original-referrer')) {
+        if (Mozilla.Cookies.hasItem('mozilla-traffic-cop-original-referrer')) {
             referrer = Mozilla.Cookies.getItem('mozilla-traffic-cop-original-referrer');
 
             // referrer shouldn't persist
             Mozilla.Cookies.removeItem('mozilla-traffic-cop-original-referrer');
+        }
+
+        return referrer;
+    };
+
+    Analytics.getOptimizelyReferrer = function() {
+        console.log('!!!! BEGIN Analytics.getOptimizelyReferrer !!!!');
+        var referrer;
+
+        // This is set by Optimizely's JS *only after a redirect*. Necessary
+        // because 'optimizely-original-referrer' is set every time Optimizely
+        // loads. We need to make sure a redirect was performed (as opposed to
+        // an in-place content change).
+        var optlyDidRedirect = Mozilla.Cookies.getItem('optimizelyRedirect');
+
+        // if a redirect was performed, and an original referrer was stored
+        if (optlyDidRedirect && Mozilla.Cookies.hasItem('optimizely-original-referrer')) {
+            console.log('Optimizely did a redirect, so we grab the original referrer...');
+            referrer = Mozilla.Cookies.getItem('optimizely-original-referrer');
+
+            // referrer shouldn't persist
+            Mozilla.Cookies.removeItem('optimizely-original-referrer');
+        }
+
+        console.log('...determined referrer to be: ' + referrer);
+
+        console.log('!!!! END Analytics.getOptimizelyReferrer !!!!');
+
+        return referrer;
+    };
+
+    Analytics.getExperimentToolReferrer = function() {
+        var referrer;
+
+        // make sure we can work with cookies
+        if (Mozilla.Cookies) {
+            // first check referrer from Traffic Cop
+            referrer = Analytics.getTrafficCopReferrer();
+
+            // if no Traffic Cop referrer exists, check Optimizely
+            if (!referrer) {
+                referrer = Analytics.getOptimizelyReferrer();
+            }
         }
 
         return referrer;
@@ -42,10 +85,12 @@ if (typeof Mozilla == 'undefined') {
             'pageId': Analytics.getPageId()
         };
 
-        var referrer = Analytics.getTrafficCopReferrer();
+
+        var referrer = Analytics.getExperimentToolReferrer();
 
         // if original referrer exists, pass it to GTM
         if (referrer) {
+            console.log('Sending this referer to GTM: ' + referrer);
             // Traffic Cop sets the referrer to 'direct' if document.referer is empty
             // prior to the redirect, so this value will either be a URL or the string 'direct'.
             dataObj.customReferrer = referrer;
--- media/js/base/optimizely-snippet.js
+++ media/js/base/optimizely-snippet.js
@@ -5,11 +5,59 @@
 (function() {
     'use strict';
 
+    console.log('**** BEGIN optimizely-snippet.js ****');
+
+    function getOptimizelyReferrer(referrer) {
+        var it = referrer || document.referrer || 'direct';
+
+        return it;
+    }
+
     var OPTIMIZELY_PROJECT_ID = document.getElementsByTagName('html')[0].getAttribute('data-optimizely-project-id');
 
     // If doNotTrack is not enabled, it is ok to add Optimizely
     // @see https://bugzilla.mozilla.org/show_bug.cgi?id=1217896 for more details
     if (typeof Mozilla.dntEnabled === 'function' && !Mozilla.dntEnabled() && OPTIMIZELY_PROJECT_ID) {
+        /*
+        var domain = 'www.mozilla.org';
+        var cookieDomain = '.' + domain;
+        var originalReferrer = Mozilla.Cookies.getItem('optimizely-original-referrer');
+
+        // if we don't have an original referrer, or document.referrer is from another
+        //if (originalReferrer === 'undefined' || document.referrer.indexOf(domain) === -1) {
+        if (originalReferrer === 'undefined') {
+            Mozilla.Cookies.setItem('optimizely-original-referrer', document.referrer, null, null, cookieDomain);
+        // if checkReferrer is not undefined and current referrer is from our domain,
+        // update the session_referrer cookie to what was previously stored
+        // (why the fuck? is this necessary at all? isn't this just updating the cookie with the same value?)
+        } else {
+            Mozilla.Cookies.setItem('optimizely-original-referrer', checkReferrer, null, null, cookieDomain);
+        }
+        */
+
+        // store the original referrer in the event Optimizely performs a redirect
+        //Mozilla.Cookies.setItem('optimizely-original-referrer', getOptimizelyReferrer());
+
+        if (Mozilla.Cookies) {
+            // check to see if referrer was already stored
+            var originalReferrer = Mozilla.Cookies.getItem('optimizely-original-referrer');
+            var optlyDidRedirect = Mozilla.Cookies.getItem('optimizelyRedirect');
+
+            console.log('Original referrer is: ' + originalReferrer);
+            console.log('Did redirect is: ' + optlyDidRedirect);
+
+            // we don't want to overwrite the referrer after a redirect if it was previously set
+            // TODO: check optlyDidRedirect here as well!
+            if (!originalReferrer && !optlyDidRedirect) {
+                var ref = getOptimizelyReferrer();
+                console.log('Updating optimizely-original-referrer to ' + ref);
+                Mozilla.Cookies.setItem('optimizely-original-referrer', ref);
+            }
+        }
+
+        console.log('**** END optimizely-snippet.js ****');
+
+        // now that referrer dance is complete, load optimizely
         (function(d, optID) {
             var newScriptTag = d.createElement('script');
             var target = d.getElementsByTagName('script')[0];
